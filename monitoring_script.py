9b22d890eeab76e67a9457e5a1385bd3a976dbe1
Fatih <turgayfatih.yasar@hotmail.com>
2025-02-05 05:42:35 +0100

    update code

--- monitoring_script.py
+++ monitoring_script.py
@@ -12,16 +12,11 @@ if not os.path.exists("reports"):
 
 def save_servers_to_json(servers):
     with open("servers.json", "w") as file:
-        json.dump(servers, file)
+        json.dump(servers, file, indent=4)
 
 def load_servers_from_json():
-    servers = {}
-    try:
-        with open("servers.json", "r") as file:
-            servers = json.load(file)
-    except FileNotFoundError:
-        pass
-    return servers
+    with open("servers.json", "r") as file:
+        return json.load(file)
 
 servers_to_check = load_servers_from_json()
 
@@ -30,18 +25,25 @@ def ping_server(server):
     update_monitoring_log(server, "online" if response_time is not None else "offline")
 
 def add_server(server):
-    servers_to_check[server] = {"checks": []}
-    save_servers_to_json(servers_to_check)
+    servers = load_servers_from_json()
+    if server not in servers:
+        servers.append(server)
+        save_servers_to_json(servers)
 
 def remove_server(server):
-    if server in servers_to_check:
-        del servers_to_check[server]
-        save_servers_to_json(servers_to_check)
+    servers = load_servers_from_json()
+    if server in servers:
+        servers.remove(server)
+        save_servers_to_json(servers)
 
 def list_servers():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    servers_to_check = load_servers_from_json()
     print("Lijst van gemonitorde servers:")
     for server in servers_to_check:
         print(server)
+    input("druk op enter om door te gaan...")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def save_logs_to_json(logs):
     with open("monitoring_log.json", "w") as file:
@@ -94,10 +96,14 @@ def start_monitoring(interval):
         while monitoring_active:
             for server in servers_to_check:
                 ping_server(server)
-            print(f"[bold green]Monitoring is actief. Volgende controle over [bold red]{interval} [/bold red]seconden...[/bold green]")
-            generate_html_report() 
-            time.sleep(interval)
-
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"\n[bold green]Monitoring is actief. Volgende controle over [bold red]{interval}[/bold red] seconden...[/bold green]\n", end="", flush=True)
+                for _ in range(interval):
+                    time.sleep(1)
+                    print(".", end="", flush=True)
+                print()
+                generate_html_report()
+            
     monitoring_active = True
     monitoring_thread = threading.Thread(target=monitoring_task)
     monitoring_thread.daemon = True
@@ -109,7 +115,7 @@ def stop_monitoring():
 
 def main_interactive():
     while True:
-        print("-----------------------------------------------------------------")
+        os.system('cls' if os.name == 'nt' else 'clear')
         print("[italic magenta]CLI commando's\npython monitoring_script.py add_server <servernaam>\npython monitoring_script.py remove_server <servernaam>\npython monitoring_script.py list_servers\npython monitoring_script.py start_monitoring <interval>\npython monitoring_script.py stop_monitoring\n[/italic magenta]")
         print("[bold cyan]1. Voeg een server toe[/bold cyan]")
         print("[bold yellow]2. Verwijder een server[/bold yellow]")
@@ -120,54 +126,63 @@ def main_interactive():
         choice = input("Voer het nummer van de actie in: ")
 
         if choice == "1":
+            os.system('cls' if os.name == 'nt' else 'clear')
             server = input("Voer de servernaam of het IP-adres in om toe te voegen: ")
             add_server(server)
             print(f"[bold green]Server {server} is toegevoegd aan de lijst van servers.[/bold green]")
         elif choice == "2":
+            os.system('cls' if os.name == 'nt' else 'clear')
             server = input("Voer de servernaam of het IP-adres in om te verwijderen: ")
             remove_server(server)
             print(f"[bold red]Server {server} is verwijderd uit de lijst van servers.[/bold red]")
         elif choice == "3":
             list_servers()
         elif choice == "4":
+            os.system('cls' if os.name == 'nt' else 'clear')
             interval = int(input("Voer het controle-interval in seconden in: "))
             start_monitoring(interval)
         elif choice == "5":
+            os.system('cls' if os.name == 'nt' else 'clear')
             stop_monitoring()
             print("[yellow]Monitoring is gestopt.[/yellow]")
         elif choice == "6":
             break
         else:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("[bold red]Ongeldige keuze. Probeer opnieuw.[/bold red]")
+            input("druk op enter om door te gaan...")
 
 def main_command_line():
     if len(sys.argv) < 2:
-        print("[bold red]Gebruik: python monitoring_script.py add_server <servernaam> / remove_server <servernaam> / list_servers / start_monitoring/ stop monitoring[/bold red]")
+        print("[bold red]Gebruik: python monitoring_script.py add <servernaam> / remove <servernaam> / list / start[/bold red]")
         return
 
     action = sys.argv[1]
 
-    if action == "add_server":
+    if action == "add":
         if len(sys.argv) == 3:
             add_server(sys.argv[2])
         else:
-            print("[bold red]Gebruik: python monitoring_script.py add_server <servernaam>[/bold red]")
-    elif action == "remove_server":
+            print("[bold red]Gebruik: python monitoring_script.py add <servernaam>[/bold red]")
+    elif action == "remove":
         if len(sys.argv) == 3:
             remove_server(sys.argv[2])
         else:
-            print("[bold red]Gebruik: python monitoring_script.py remove_server <servernaam>[/bold red]")
-    elif action == "list_servers":
+            print("[bold red]Gebruik: python monitoring_script.py remove <servernaam>[/bold red]")
+    elif action == "list":
         list_servers()
-    elif action == "start_monitoring":
+    elif action == "start":
         if len(sys.argv) == 3:
             interval = int(sys.argv[2])
-            print("OK")
             start_monitoring(interval)
-    elif action == "stop_monitoring":
-            stop_monitoring()
+            try:
+                while monitoring_active:
+                    time.sleep(1)
+            except KeyboardInterrupt:
+                stop_monitoring()
+                print("\n[bold red]Monitoring gestopt.[/bold red]")
     else:
-        print("[bold red]Ongeldige actie. Gebruik: python monitoring_script.py add_server <servernaam> / remove_server <servernaam> / list_servers / start_monitoring / stop_monitoring![/bold red]")
+        print("[bold red]Ongeldige actie. Gebruik: python monitoring_script.py add <servernaam> / remove <servernaam> / list / start[/bold red]")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
